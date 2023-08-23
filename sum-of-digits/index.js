9af29f6f66d4dfddc2aa03fcda5ee2d3d54f8ee5
Laya Aminosharei <laya.aminosharei@gmail.com>
2023-08-23 10:37:13 +0200

    update challenge sum of digits

--- sum-of-digits/index.js
+++ sum-of-digits/index.js
@@ -1,13 +1,27 @@
 function digitalRoot(n) {
   
+let stringArray = n.toString().split('');
+let digitsArray = stringArray.map(Number)
+let sum = 0;
+  
+  while(digitsArray.length >1){
+    sum= digitsArray.reduce((a,b)=> a+b,0)
+    stringArray = sum.toString().split('');
+    digitsArray = stringArray.map(Number);
+  }
+  return sum;
+}
+
+
+// shorter solution using unary plus operator:
+function digitalRoot(n) {
+  
     let stringArray = n.toString().split('');
-    let digitsArray = stringArray.map(Number)
     let sum = 0;
       
-      while(digitsArray.length >1){
-        sum= digitsArray.reduce((a,b)=> a+b,0)
+      while(stringArray.length >1){
+        sum= stringArray.reduce((a,b)=> +a + +b,0)
         stringArray = sum.toString().split('');
-        digitsArray = stringArray.map(Number);
       }
       return sum;
     }
