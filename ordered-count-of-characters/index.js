6ca283dd8114c0597e9c71eb23f581ed7d90c1a1
Laya Aminosharei <laya.aminosharei@gmail.com>
2023-12-29 12:49:04 +0100

    ordered count of characters

---
+++ ordered-count-of-characters/index.js
@@ -0,0 +1,15 @@
+
+const orderedCount = function (text) {
+    const charsMap = new Map();
+    for(let i = 0 ; i < text.length ; i++){
+        // if the key doesn't exist in map, add it with value 1
+        // if it exists, increment by 1
+      charsMap.set(text[i], (charsMap.get(text[i]) ?? 0) + 1)
+    }
+    
+    let result= []
+    for(entry of charsMap.entries()){
+      result.push(entry)
+    }
+    return result
+  }
