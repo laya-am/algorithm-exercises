ace85a315b7cd0f216da231f1578f4ce73eb1ea2
Laya Aminosharei <laya.aminosharei@gmail.com>
2024-01-12 17:09:17 +0100

    add better solution for two-sum

--- two-sum/index.js
+++ two-sum/index.js
@@ -1,6 +1,6 @@
+//   solution 1: brute force, not efficient but easy to implement
 function twoSum(numbers, target) {
       
-    //   solution 2: not efficient but easy to implement
       for (let i=0 ; i< numbers.length-1; i++){
         for (let j= i+1; j<numbers.length; j++){
           if(numbers[i] + numbers[j] === target){
@@ -9,3 +9,19 @@ function twoSum(numbers, target) {
         }
       }
     }
+
+    // solution 2: 
+
+function twoSum2(nums, target) {
+  //if the complimentary num is not in the hash, add num with the index of the num
+  //otherwise, return the index of num and the index of the complimentary num
+  const hash = new Map();
+  for (i = 0 ; i < nums.length ; i++) {
+    const compliNum = target - nums[i];
+    if(hash.get(compliNum) === undefined){ 
+      hash.set(nums[i], i)
+    }else{
+      return [i , hash.get(compliNum)]
+    }
+  }
+};
